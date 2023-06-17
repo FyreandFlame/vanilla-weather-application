4752f7efb4c2d7608da02d1ba39e376b13f83a68
FyreandFlame <128990066+FyreandFlame@users.noreply.github.com>
2023-06-17 22:42:34 +0100

    included time

--- source/app.js
+++ source/app.js
@@ -1,15 +1,32 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+  hours = `0${hours}`
+}
+let minutes = date.getMinutes();
+if (minutes < 10){
+  minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response){
  let currentTemperature = document.querySelector("#temperature");
  let currentCity = document.querySelector("#city");
 let currentWeather = document.querySelector("#description");
 let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#wind");
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = formatDate(response.data.time*1000);
 windSpeed.innerHTML = Math.round(response.data.wind.speed);
 currentWeather.innerHTML = response.data.condition.description;
 humidity.innerHTML = response.data.temperature.humidity;
 currentCity.innerHTML = response.data.city;
 currentTemperature.innerHTML = Math.round(response.data.temperature.current)
-console.log(response.data);
 }
 
 
